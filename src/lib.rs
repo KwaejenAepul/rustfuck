2c3c892c293f5a1c5c9fbd349e6d4098e9529dc2
KwaejenAepul <66973949+KwaejenAepul@users.noreply.github.com>
2021-12-30 16:18:06 +0100

    Start of rewrite Everything is broken :)

--- src/lib.rs
+++ src/lib.rs
@@ -1,60 +1,61 @@
 use std::{env, fs};
+/*
+needs:  - input
+        - output
+        - print
+        - loop
+*/ 
 
-#[derive(Debug)]
-pub struct BrainfuckFile {
-    pub char_vector: Vec<char>,
+pub fn read_file(mut args: env::Args) -> String {
+    args.next();
+    let file = args.next().unwrap();
+    let contents = fs::read_to_string(file).unwrap();
+    contents
 }
-impl BrainfuckFile {
-    pub fn read_file(mut args: env::Args) -> BrainfuckFile {
-        args.next();
-        let file = args.next().unwrap();
-        let contents = fs::read_to_string(file).unwrap();
-        let contents: String = contents.trim().parse().unwrap();
-        let mut char_vec = Vec::new();
-        for i in contents.chars() {
-            match i {
-                '>' => char_vec.push(i),
-                '<' => char_vec.push(i),
-                '+' => char_vec.push(i),
-                '-' => char_vec.push(i),
-                '.' => char_vec.push(i),
-                ',' => char_vec.push(i),
-                '[' => char_vec.push(i),
-                ']' => char_vec.push(i),
-                _ => continue,
-            };
-        }
-        BrainfuckFile {
-            char_vector: char_vec,
-        }
-    }
-    pub fn interpert_bf(char_vec: &Vec<char>) {
-        let mut i: usize = 0;
-        let mut cell_array = [0u8; 30000];
-        let mut cell_pointer: usize = 0;
-        let mut squarebracket_index_vec: Vec<u32> = vec![];
-        while i < char_vec.len() {
-            match char_vec[i] {
-                '>' => cell_pointer = cell_pointer + 1,
-                '<' => cell_pointer = cell_pointer - 1,
-                '+' => cell_array[cell_pointer] = cell_array[cell_pointer] + 1,
-                '-' => cell_array[cell_pointer] = cell_array[cell_pointer] - 1,
-                '.' => {println!("{:?}", cell_array[i])},
-                ',' => continue,
-                '[' => squarebracket_index_vec.push(i as u32),
-                ']' => {
-                    if cell_array[cell_pointer] != 0 {
-                        if squarebracket_index_vec.len() == 0{
-                            panic!("\nno start of loop \nerror at character index: {}", i);
-                        }
-                        i = squarebracket_index_vec[squarebracket_index_vec.len() - 1] as usize
-                    }else {
-                        squarebracket_index_vec.pop();
+ pub fn interpert_bf(bf: String){
+     let mut data_cell_array = [0u32; 30000];
+     let mut cursor: usize = 0;
+     let mut i = 0;
+     let mut loops_vec: Vec<u8> = Vec::new();
+     while i < bf.len(){
+         match bf.chars().nth(i).unwrap(){
+             '>' =>{
+                cursor += 1;
+                i += 1;
+             },
+             '<' =>{
+                cursor -= 1;
+                i += 1;
+             },
+             '+' =>{
+                data_cell_array[cursor] += 1;
+                i += 1;
+             },
+             '-' =>{
+                data_cell_array[cursor] += 1;
+                i += 1;
+             },
+             '.' =>{
+                 println!("{:?}", bf.chars().nth(i).unwrap())
+
+             },
+             ',' =>{
+
+             },
+             '[' =>{
+                loops_vec.push(i as u8)
+             },
+             ']' =>{
+                if data_cell_array[cursor] != 0 {
+                    if loops_vec.len() == 0{
+                        panic!("\nno start of loop \nerror at character index: {}", i);
                     }
-                }
-                _ => continue,
-            };
-            i = i +1;
-        }
-    }
-}
+                    i = loops_vec[loops_vec.len() - 1] as usize
+                 }else{
+                     loops_vec.pop();
+                 }
+             },
+             _ => {i += 1},
+         };
+     }
+ }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 
 fn main() {
-    let structboy = rustfuck::BrainfuckFile::read_file(env::args());
-    rustfuck::BrainfuckFile::interpert_bf(&structboy.char_vector);
+    rustfuck::interpert_bf(rustfuck::read_file(env::args()));
 }
+
