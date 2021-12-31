41a0fbbbbc87e8ecac97a41db8a1fad5d8a6365e
KwaejenAepul <66973949+KwaejenAepul@users.noreply.github.com>
2021-12-31 21:58:59 +0100

    Shit's less broken
    shit's broken but not as bad as before

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ needs:  - input
         - output
         - print
         - loop
-*/ 
+*/
 
 pub fn read_file(mut args: env::Args) -> String {
     args.next();
@@ -12,50 +12,53 @@ pub fn read_file(mut args: env::Args) -> String {
     let contents = fs::read_to_string(file).unwrap();
     contents
 }
- pub fn interpert_bf(bf: String){
-     let mut data_cell_array = [0u32; 30000];
-     let mut cursor: usize = 0;
-     let mut i = 0;
-     let mut loops_vec: Vec<u8> = Vec::new();
-     while i < bf.len(){
-         match bf.chars().nth(i).unwrap(){
-             '>' =>{
+pub fn interpert_bf(bf: String) {
+    let mut data_cell_array = [0u32; 30000];
+    let mut cursor: usize = 0;
+    let mut i = 0;
+    let mut loops_vec: Vec<u8> = Vec::new();
+    while i < bf.len() {
+        println!("{}{}", bf.chars().nth(i).unwrap(), i);
+        match bf.chars().nth(i).unwrap() {
+            '>' => {
                 cursor += 1;
                 i += 1;
-             },
-             '<' =>{
+            }
+            '<' => {
                 cursor -= 1;
                 i += 1;
-             },
-             '+' =>{
+            }
+            '+' => {
                 data_cell_array[cursor] += 1;
                 i += 1;
-             },
-             '-' =>{
+            }
+            '-' => {
                 data_cell_array[cursor] += 1;
                 i += 1;
-             },
-             '.' =>{
-                 println!("{:?}", bf.chars().nth(i).unwrap())
-
-             },
-             ',' =>{
-
-             },
-             '[' =>{
-                loops_vec.push(i as u8)
-             },
-             ']' =>{
+            }
+            '.' => {
+                println!("{:?}", bf.chars().nth(i).unwrap());
+                i += 1;
+            }
+            ',' => {
+                i += 1;
+            }
+            '[' => {
+                loops_vec.push(i as u8);
+                i += 1;
+            }
+            ']' => {
                 if data_cell_array[cursor] != 0 {
-                    if loops_vec.len() == 0{
+                    if loops_vec.len() == 0 {
                         panic!("\nno start of loop \nerror at character index: {}", i);
                     }
                     i = loops_vec[loops_vec.len() - 1] as usize
-                 }else{
-                     loops_vec.pop();
-                 }
-             },
-             _ => {i += 1},
-         };
-     }
- }
+                }else {
+                    loops_vec.pop();
+                    i += 1;
+                }
+            }
+            _ => i += 1,
+        };
+    }
+}
